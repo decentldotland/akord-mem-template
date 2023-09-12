34dd9ec3941db511e50dca44b7710b0a3beec27e
Max Knivets <knivets.m@gmail.com>
2023-09-12 19:52:37 -0400

    chore: upgrade types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,27 @@
-export interface Config {
+export enum GatingType {
+  ERC20 = "erc20",
+  ERC721 = "erc721",
+}
+
+export interface AkordContainerConfig {
   vault_id: string;
-  gating_type: string;
+  gating_type: GatingType;
   token_address: string;
   max_entries: number;
   token_threshold: number;
   akord_address: string;
 }
+
+export interface createContainerArgs extends AkordContainerConfig {}
+
+export interface JoinContainerConfig {
+  akord_address: string;
+  container_id?: string;
+  vault_id?: string;
+}
+
+export type AkordContainerConfigKeys = keyof AkordContainerConfig;
+export type JoinContainerConfigKeys = keyof JoinContainerConfig;
 
 export interface Member {
   evm_address: string;
@@ -14,7 +30,7 @@ export interface Member {
 
 export interface Container {
   id: string;
-  config: Config;
+  config: AkordContainerConfig;
   controller_address: string;
   // epoch time
   creation_timestamp: number;
