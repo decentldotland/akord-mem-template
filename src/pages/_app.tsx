cca7289c499b9841d67fc40653ce55a934851ff8
Max Knivets <knivets.m@gmail.com>
2023-09-12 19:53:35 -0400

    fix: add toast + refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,54 +1,38 @@
 import { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 import Head from "next/head";
 
-import dynamic from "next/dynamic";
+import { Toaster } from "react-hot-toast";
 
 const ConfigWrappers = dynamic(() => import("@/components/wrapper"), {
   ssr: false,
 });
 
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
     <>
       <Head>
         <title>Akord TGA POC</title>
-        <meta
-          name="description"
-          content={`Akord TGA POC`}
-        />
+        <meta name="description" content={`Akord TGA POC`} />
         <meta name="twitter:card" content="summary"></meta>
-        <meta
-          name="twitter:image"
-          content={`https://akord.com/favicon.ico`}
-        />
+        <meta name="twitter:image" content={`https://akord.com/favicon.ico`} />
         <meta name="twitter:title" content={`Akord TGA POC`} />
         <meta name="twitter:url" content={`https://akord.com`}></meta>
-        <meta
-          name="twitter:description"
-          content={`Akord TGA POC`}
-        />
+        <meta name="twitter:description" content={`Akord TGA POC`} />
 
         <meta property="og:card" content="summary" />
-        <meta
-          property="og:image"
-          content={`https://akord.com/favicon.ico`}
-        />
+        <meta property="og:image" content={`https://akord.com/favicon.ico`} />
         <meta property="og:title" content={`Akord TGA POC`} />
         <meta property="og:url" content={`https://akord.com/`} />
-        <meta
-          property="og:description"
-          content={`Akord TGA POC`}
-        />
+        <meta property="og:description" content={`Akord TGA POC`} />
       </Head>
       <ConfigWrappers>
-
-      <Component {...pageProps} />
+        <Component {...pageProps} />
+        <Toaster />
       </ConfigWrappers>
-
     </>
   );
 }
