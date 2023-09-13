90290bf1e181637a71a234ae0367bf8d22998b0a
Max Knivets <knivets.m@gmail.com>
2023-09-12 20:12:17 -0400

    feat: refactor and complete website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,94 +1,104 @@
-import { Akord, Auth } from "@akord/akord-js";
+import { Akord } from "@akord/akord-js";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import { useAccount } from "wagmi";
 
+import { MockMEMState, adminMessage, userMessage } from "@/constants";
 import { createContainer, joinContainer, readMEM } from "@/helpers";
 import { createSignature } from "@/helpers/signature";
-import { MEMState } from "@/types";
+import { AkordContainerConfig, MEMState } from "@/types";
+
+import CodeLinks from "@/components/codelinks";
+import CodePreview from "@/components/codepreview";
+import { AkordSignIn } from "@/components/akordSignIn";
+import { AkordActions } from "@/components/akordActions";
+import { NotWhitelisted, UnitializedState } from "@/components/warnings";
 
 const ConnectButton = dynamic(() =>
   import("@rainbow-me/rainbowkit").then((mod) => mod.ConnectButton)
 );
 
-const emptyConfig = {
-  vault_id: "",
-  gating_type: "",
-  token_address: "",
-  max_entries: 1,
-  token_threshold: 1000000,
-  akord_address: "",
-};
-
 export default function Home() {
-  const { address } = useAccount();
-
-  // Akord
   const [akord, setAkord] = useState<Akord>();
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
+  const { address: ethAddress } = useAccount();
 
   // state and handlers
-  const [state, setState] = useState<MEMState>();
+  const [state, setState] = useState<MEMState>(MockMEMState);
+  const [stateInit, setStateInit] = useState<boolean>(true);
   const [statusMessage, setStatusMessage] = useState<string | undefined>();
+  const stateAndAddressExist = !!(state && ethAddress);
 
-  async function akordSignIn() {
-    if (!email || !password) return;
-    const { wallet } = await Auth.signIn(email, password);
-    Akord.init(wallet).then(setAkord).catch(console.log);
-  }
-
-  useEffect(() => {
-    readMEM().then(setState).catch(console.log);
-  }, []);
-
-  async function handleCreateContainer() {
+  // create new akord container
+  async function handleCreateContainer(config: AkordContainerConfig) {
     setStatusMessage(undefined);
-    if (!state || !address) return;
-    const user_signature = await createSignature(
-      `akord-mem::${state.users_counter}`
-    );
-    const admin_signature = await createSignature(
-      `akord-mem::${state.admin_counter}`
-    );
-    const newState = await createContainer(
-      address,
-      emptyConfig,
+    if (!stateAndAddressExist) return;
+
+    let user_signature = "";
+    let admin_signature = "";
+
+    // generate user signatures
+    try {
+      user_signature = await createSignature(
+        `${userMessage}${state.users_counter}`
+      );
+      admin_signature = await createSignature(
+        `${adminMessage}${state.admin_counter}`
+      );
+    } catch (e) {
+      toast.error("Signature generation failed", { duration: 3000 });
+      return;
+    }
+
+    // call the createContainer helper function directly connected to MEM
+    const newMEMState = await createContainer(
+      ethAddress,
+      config,
       user_signature,
       admin_signature
     );
-    if (newState) {
-      const { containers } = newState;
+
+    if (newMEMState) {
+      setState(newMEMState);
+      const { containers } = newMEMState;
       const lastContainer = containers[containers.length - 1];
-      setStatusMessage(
-        `Container created successfully! Container object: ${JSON.stringify(
-          lastContainer
-        )}`
+      toast.success(
+        `Container ${lastContainer.id.substr(0, 8)}... created successfully!`,
+        { duration: 3000 }
       );
     } else {
-      setStatusMessage("Failed to create container.");
+      toast.error("Failed to create container.", { duration: 3000 });
     }
   }
 
-  async function handleJoinContainer(
+  async function handleJoinContainer({
     akord_address = "",
     container_id = "",
-    vault_id = ""
-  ) {
+    vault_id = "",
+  }) {
     // status cleanup and validation
     setStatusMessage(undefined);
-    if (!state || !address) return;
+    if (!stateAndAddressExist) return;
 
-    // getting args
-    const user_signature = await createSignature(
-      `akord-mem::${state.users_counter}`
-    );
-    const admin_signature = await createSignature(
-      `akord-mem::${state.admin_counter}`
-    );
+    let user_signature = "";
+    let admin_signature = "";
 
-    const newState = await joinContainer(
-      address,
+    // generate user signatures
+    try {
+      user_signature = await createSignature(
+        `${userMessage}${state.users_counter}`
+      );
+      admin_signature = await createSignature(
+        `${adminMessage}${state.admin_counter}`
+      );
+    } catch (e) {
+      toast.error("Signature generation failed", { duration: 3000 });
+      return;
+    }
+    console.log(user_signature);
+    // call the joinContainer helper function directly connected to MEM
+    const newMEMState = await joinContainer(
+      ethAddress,
       akord_address,
       user_signature,
       admin_signature,
@@ -96,87 +106,55 @@ export default function Home() {
       vault_id
     );
 
-    if (newState) {
-      const { containers } = newState;
+    if (newMEMState) {
+      setState(newMEMState);
+      const { containers } = newMEMState;
       const lastContainer = containers[containers.length - 1];
-      setStatusMessage(
-        `Container joined successfully! Container object: ${JSON.stringify(
-          lastContainer
-        )}`
+      toast.success(
+        `Container joined successfully! ID: ${lastContainer.id.substr(0, 8)}.`,
+        { duration: 3000 }
       );
     } else {
-      setStatusMessage("Failed to join container.");
+      toast.error("Failed to join container.", { duration: 3000 });
     }
   }
 
+  // read and set MEM state object
+  useEffect(() => {
+    readMEM().then(setState).catch(console.log);
+  }, []);
+
+  // when the state initializes, you need to interact a few times with it so
+  // MEM displays it, this effect adds a mock state for uninitialized states
+  useEffect(() => {
+    if (!state) {
+      setState(MockMEMState);
+      setStateInit(false);
+    } else setStateInit(true);
+  }, [state]);
+
   return (
     <main className="flex flex-col items-center justify-center gap-y-6 min-h-screen p-24">
-      <h1 className="text-black text-4xl font-bold">
+      <h1 className="text-black text-4xl font-bold text-center">
         Akord Token-Gated Access Proof of Concept
       </h1>
-      <div className="flex gap-x-4">
-        <a
-          className="underline hover:text-blue-300"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://gist.github.com/charmful0x/7908734530a8551c11b02e0b553abbd3#file-akord-js"
-        >
-          Contract File ↗
-        </a>
-        <a
-          className="underline hover:text-blue-300"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://gist.github.com/charmful0x/7908734530a8551c11b02e0b553abbd3#file-akord-json"
-        >
-          State File ↗
-        </a>
-      </div>
-      <h2 className="font-bold text-center"> Application state </h2>
-      <div className="border-2 border-black max-h-[200px] overflow-y-scroll p-2">
-        <pre>
-          <code>{JSON.stringify(state, null, 4)}</code>
-        </pre>
-      </div>
-      <ConnectButton />
-      <form
-        className="flex flex-row gap-x-3 items-center justify-center"
-        onSubmit={(e) => {
-          e.preventDefault();
-          akordSignIn();
-        }}
-      >
-        <input
-          placeholder="email"
-          className="border-2 border-black px-2 py-1"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          className="border-2 border-black px-2 py-1"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="border-2 border-black px-2 py-1" type="submit">
-          Akord Sign In
-        </button>
-      </form>
-      {address && (
-        <>
-          <button
-            className="border-black border-2 px-2 py-1 hover:shadow-lg"
-            onClick={() => handleCreateContainer()}
-          >
-            Create a new container
-          </button>
-          <button
-            className="border-black border-2 px-2 py-1 hover:shadow-lg"
-            onClick={() => handleJoinContainer()}
-          >
-            Join Container
-          </button>
-        </>
+      <CodeLinks />
+      {!stateInit && <UnitializedState />}
+      {ethAddress && state && state.admin_address !== ethAddress && (
+        <NotWhitelisted address={ethAddress} />
       )}
+      <CodePreview state={state} />
+      <ConnectButton />
+      {ethAddress && (
+        <AkordActions
+          {...{
+            stateAndAddressExist,
+            handleCreateContainer,
+            handleJoinContainer,
+          }}
+        />
+      )}
+      <AkordSignIn {...{ setAkord }} />
     </main>
   );
 }
