5890e3d57f76e00fd9b7f7c122172564a7902d57
Max Knivets <knivets.m@gmail.com>
2023-09-13 23:01:57 -0400

    feat: createNodeSignature function

--- src/helpers/signature.ts
+++ src/helpers/signature.ts
@@ -1,4 +1,6 @@
-import { createWalletClient, custom } from "viem";
+import axios from "axios";
+import { createWalletClient, custom, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
 import { mainnet } from "viem/chains";
 
 export async function createSignature(message: string) {
@@ -11,6 +13,36 @@ export async function createSignature(message: string) {
   // get user to sign signature
   const [account] = await walletClient.getAddresses();
   const signature = await walletClient.signMessage({
+    account,
+    message,
+  });
+
+  return signature;
+}
+
+// used for ensuring the user request originates on our frontend
+export async function requestAdminSignature(message: string) {
+  try {
+    const response = (await axios.post("/api/get-admin-sig", { message })).data;
+    return response;
+  } catch (e: any) {
+    console.log(e.message);
+    return undefined;
+  }
+}
+
+export async function createNodeSignature(message: string) {
+  const pk = ("0x" + process.env.ETH_ADMIN_PK) as `0x${string}`;
+  if (!pk) return undefined;
+  const account = privateKeyToAccount(pk);
+
+  const client = createWalletClient({
+    account,
+    chain: mainnet,
+    transport: http(),
+  });
+
+  const signature = await client.signMessage({
     account,
     message,
   });
